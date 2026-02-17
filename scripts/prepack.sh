029a25fd8841b959129c7bce58ea458323383c19
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-02-17 14:04:03 -0600

    fix(scripts): include the lib directory in the output zip
    in the last release additional files were included in the repository
    that are necessary for the application to function, but they were not
    included in the build script. The lib directory was excluded from the
    final zip output which was preventing the application from running
    correctly
    
    Fixes: #12

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p dist
 npm ci --prod
-zip dist/mezmo-cloudwatch.zip -r node_modules/ config.js index.js package.json
+zip dist/mezmo-cloudwatch.zip -r node_modules/ lib/ config.js index.js package.json
